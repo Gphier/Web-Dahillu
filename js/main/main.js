13472822317da84856fde10bfed3238a1bc50a1c
Guian Liza <lguianphier@gmail.com>
2019-12-17 01:03:30 -0500

    Cambios flex

--- js/main/main.js
+++ js/main/main.js
@@ -10,9 +10,17 @@ $(document).ready(function () {
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
-      });
+    });
     /* Play Btn */
     $('.modal').modal({
         dismissible: true,
+    });
+    $('.materialboxed').materialbox();
+    var cols = document.querySelectorAll('.grid-container');
+    // console.log(cols);
+    [].forEach.call(cols, (e) => {
+        for (var i = e.children.length; i >= 0; i--) {
+            e.appendChild(e.children[Math.random() * i | 0]);
+        }
     });
 });
